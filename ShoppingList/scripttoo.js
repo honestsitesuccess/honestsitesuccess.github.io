7cd87442a3929155c790de9c3f37e73e54dc9139
mikeharmanos <mike@honestsitesuccess.com>
2013-11-04 19:52:37 -0800

    v 1.5
    Hello document ready function, hello console log, goodbye alert

--- ShoppingList/scripttoo.js
+++ ShoppingList/scripttoo.js
@@ -1,8 +1,6 @@
 // Shopping List for Mike Harmanos, Honest Site Success.com, 2013
 
-$(document).ready(function() {
-	var form = document.getElementById('storelist');
-
+var form = document.getElementById('storelist');
 
 form.onsubmit = checkscript;
 
@@ -10,22 +8,23 @@ function checkscript (e) {
     var item = document.getElementById('item').value;
     var date = document.getElementById('date').value;
     var store = document.getElementById('store').value;
-    if (item == "") {
-		// no item listed
-		console.log("Please insert an item.");
-		return false;
-	} else if (date == "") {
-		// no date listed
-		console.log("Please insert a date.");
-		return false;
-	}
-    //Execute whatever code comes next
-    nextStep(item,date,store);
-	return false;
+    if (item === "") {
+// no item listed
+        alert("Please insert an item.");
+        return false;
+    }
+    if (date === "") {
+// no date listed
+        alert("Please insert a date.");
+        return false;
+    }
+//Execute whatever code comes next
+    nextStep(item, date, store);
+        return false;
 }
 
 function nextStep(item, date, store) {
-    console.log("Thanks! You want: "+item+" on "+date+" from " +store);
+    alert("Thanks! You want: "+item+" on "+date+" from " +store);
 	addItem();
 }
 
